5250008c17c15301c254fb9b1758276b1d562439
Matthew Machurek <mrjm2005@yahoo.com>
2020-11-30 14:55:21 -0600

    code is on vs but computer slow down issue

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -8,6 +8,7 @@ import { LocationList } from "./location/LocationList"
 import { AnimalList } from "./animal/AnimalList"
 import { CustomerList } from "./customer/CustomerList"
 import { EmployeeList } from "./employee/EmployeeList"
+import { EmployeeForm } from "./employee/EmployeeForm"
 
 export const ApplicationViews = (props) => {
     return (
@@ -38,10 +39,20 @@ export const ApplicationViews = (props) => {
             </CustomerProvider>
 
             <EmployeeProvider>
-                {/* Render the animal list when http://localhost:3000/employees */}
-                <Route exact path="/employees">
-                    <EmployeeList />
-                </Route>
+                <AnimalProvider>
+                    <LocationProvider>
+                        {/* Render the animal list when http://localhost:3000/employees */}
+                        <Route exact path="/employees" render={
+                            props => {
+                                return < EmployeeList {...props} />
+                            }
+                        } />
+
+                        <Route exact path="/employees/create" render={
+                            props => <EmployeeForm {...props} />
+                        } />
+                    </LocationProvider>
+                </AnimalProvider>
             </EmployeeProvider>
         </>
     )
