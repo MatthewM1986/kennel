d7979925e7285833bddb16245a61e31020455a1a
Matthew Machurek <mrjm2005@yahoo.com>
2020-12-01 14:02:38 -0600

    location names are now hyperlinks

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect } from "react"
 import { LocationContext } from "./LocationProvider"
 import { Location } from "./Location"
+import { Link } from "react-router-dom"
 import "./Location.css"
 
-export const LocationList = () => {
+export const LocationList = (props) => {
     // This state changes when `getLocations()` is invoked below
     const { locationsArray, getLocations } = useContext(LocationContext)
 
@@ -22,15 +23,25 @@ export const LocationList = () => {
         it is responding to is that the location state changed.
     */
     //useEffect(() => {
-        // console.log("LocationList: Location state changed")
-        // console.log(locations)
+    // console.log("LocationList: Location state changed")
+    // console.log(locations)
     //}, [locationsArray])
 
     return (
         <div className="locations">
-        {
-            locationsArray.map(locationTaco => <Location key={locationTaco.id} locationObj={locationTaco} />)
-        }
+            <h1>Locations</h1>
+            <button onClick={() => props.history.push("/locations/create")}>
+                Add Location
+    </button>
+            <article className="locationList">
+                {
+                    locationsArray.map(locationTaco => {
+                        return <Link key={locationTaco.id} to={`/locations/${locationTaco.id}`} >
+                            <h3>{locationTaco.name}</h3>
+                        </Link>
+                    })
+                }
+            </article>
         </div>
     )
 }
