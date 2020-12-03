4ad1f8f36caf87f146af04e520bd1c5c2b7592bc
Matthew Machurek <mrjm2005@yahoo.com>
2020-12-03 10:16:59 -0600

    an animal now deletes and goes back to animal page

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -19,16 +19,14 @@ export const AnimalDetails = (props) => {
             <div className="animal__breed">{animal.breed}</div>
             <div className="animal__location">Location: {animal.location.name}</div>
             <div className="animal__owner">Customer: {animal.customer.name}</div>
-            <button onClick={
-                () => {
-                    releaseAnimal(animal)
+            <button className="btn--release"
+                onClick={() => {
+                    releaseAnimal(animal.id)
                         .then(() => {
                             props.history.push("/animals")
                         })
-                }
-            }>
-                Release Animal
-            </button>
+                }}
+            >Release</button>
             <button onClick={() => {
                 props.history.push(`/animals/edit/${animal.id}`)
             }}>Edit</button>
